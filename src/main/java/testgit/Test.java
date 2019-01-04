a378d9a5e346a44a0bc39b790732380cf2517f32
1485535675github <1485535675@qq.com>
2019-01-04 09:08:11 +0800

    第一次提交项目文件

---
+++ src/main/java/testgit/Test.java
@@ -0,0 +1,19 @@
+package testgit;
+
+/**
+ * @version V1.0
+ * @Author 罗玲红
+ * @Description: TODO
+ * @Date 2019/1/3 17:34
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+        test();
+        System.out.println("马上要下班啦！！");
+    }
+
+    public static void test(){
+        System.out.println("噜啦啦噜啦啦噜啦啦！！！！");
+    }
+}
