8150bbbc4b3f952770d9878e9b6b7b34e64d8d97
1485535675github <45547365+1485535675github@users.noreply.github.com>
2019-01-04 15:23:09 +0800

    Update Test.java

--- src/main/java/testgit/Test.java
+++ src/main/java/testgit/Test.java
@@ -9,7 +9,7 @@ package testgit;
 public class Test {
     public static void main(String[] args) {
         test();
-        System.out.println("！！！！！！！！！！！！！！");
+        System.out.println("===============================");
         System.out.println("测试测试测试");
         System.out.println("!!!!!!!!!!!!!!!!");
         System.out.println("！！ 78777777happy happy！！！");
